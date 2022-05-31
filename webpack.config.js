1d94c83f5c5eff3ecc128bada825879c20cda703
Pierre-Henri HP <pierre.henri@aptero.co>
2022-05-31 11:50:46 +0200

    Keep classname and Fname in build

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,13 @@ const webpack = require('webpack');
 const PrettierPlugin = require("prettier-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const getPackageJson = require('./scripts/getPackageJson');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const {
-  version,
-  name,
-  license,
-  repository,
-  author,
+    version,
+    name,
+    license,
+    repository,
+    author,
 } = getPackageJson('version', 'name', 'license', 'repository', 'author');
 
 const banner = `
@@ -25,68 +23,57 @@ const banner = `
 `;
 
 module.exports = {
-  mode: "production",
-  devtool: 'source-map', //for prod
-  //devtool: 'eval-source-map', //for debug
-  entry:{
-    index: path.join(__dirname, './src/lib/index.ts'),
-  },
-  output: {
-    globalObject: 'this',//https://stackoverflow.com/questions/64639839/typescript-webpack-library-generates-referenceerror-self-is-not-defined
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
-    library: "axolotis-player",
-    asyncChunks: false,
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    clean: true
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({ extractComments: false }),
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorOptions: {
-          map: {
-            inline: false
-          }
-        }
-      })
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(m|j|t)s$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: "css-loader", options: { sourceMap: true } },
+    mode: "production",
+    devtool: 'source-map', //for prod
+    //devtool: 'eval-source-map', //for debug
+    entry: {
+        index: path.join(__dirname, './src/lib/index.ts'),
+    },
+    output: {
+        globalObject: 'this',//https://stackoverflow.com/questions/64639839/typescript-webpack-library-generates-referenceerror-self-is-not-defined
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
+        library: "axolotis-player",
+        asyncChunks: false,
+        libraryTarget: 'umd',
+        umdNamedDefine: true,
+        clean: true
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false, terserOptions: {
+                    //https://github.com/terser/terser#minify-options
+                    keep_classnames: true,
+                    keep_fnames:true
+                }
+            })
         ],
-      }
-    ]
-  },
-  plugins: [
-    new PrettierPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/index.css'
-    }),
-    new webpack.BannerPlugin(banner)
-  ],
-  resolve: {
-    alias: {
-      "@root": path.resolve("./src")
     },
-    fallback: {
-      'fs': false,
-      'path': false, // ammo.js seems to also use path
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     },
-    extensions: ['.ts', '.js', '.json']
-  }
+    plugins: [
+        new PrettierPlugin(),
+        new webpack.BannerPlugin(banner)
+    ],
+    resolve: {
+        alias: {
+            "@root": path.resolve("./src")
+        },
+        fallback: {
+            'fs': false,
+            'path': false, // ammo.js seems to also use path
+        },
+        extensions: ['.ts', '.js', '.json']
+    }
 };
